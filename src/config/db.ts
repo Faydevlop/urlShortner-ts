f65f3e4566cf6fa8b89ebf1fd820019136399a22
fayis <fayfayis74@gmail.com>
2025-03-11 12:55:21 +0530

    url issue fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || "", );
+    await mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://fayfayis74:fayfayis7227@urlshortner.57uau.mongodb.net/?retryWrites=true&w=majority&appName=urlshortner", );
     console.log("MongoDB Connected here");
   } catch (error) {
     console.error("Database connection failed", error);
